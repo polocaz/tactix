eca9972d62ac240945b561ca872dbb36c2029388
Polo <cazpolo@gmail.com>
2025-11-13 00:16:22 -0500

    TACTIX-12 : Integrate SDL2 and ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,104 @@
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <stdio.h>
 
-int main() {
-    std::cout << "NationSim initialized." << std::endl;
+int main(int argc, char* argv[])
+{
+    // Make compiler happy
+    (void)argc;
+    (void)argv;
+
+
+    // -------------------------
+    // SDL2 Initialization
+    // -------------------------
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Set OpenGL attributes (version 3.3 core)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    // Create SDL window
+    SDL_Window* window = SDL_CreateWindow(
+        "Tactix",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
+    );
+
+    if (!window) {
+        printf("Error creating SDL window: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
+    // -------------------------
+    // ImGui Initialization
+    // -------------------------
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
+    ImGui::StyleColorsDark();
+
+    // -------------------------
+    // Main Loop
+    // -------------------------
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // Example ImGui window
+        ImGui::Begin("Hello Tactix!");
+        ImGui::Text("This is a working SDL2 + ImGui setup.");
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, 1280, 720);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    // -------------------------
+    // Cleanup
+    // -------------------------
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
 
